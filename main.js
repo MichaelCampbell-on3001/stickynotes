57e6fa96a981c0c57482dbd789b5f337620fa650
MichaelCampbell2020 <cam3000@gmail.com>
2022-01-29 15:02:21 -0800

    create new note functionality

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-const notesComtainer = document.getElementById("app");
-const addNoteButton = notesComtainer.querySelector(".add-note");
+const notesContainer = document.getElementById("app");
+const addNoteButton = notesContainer.querySelector(".add-note");
+
+getNotes().forEach((note) => {
+  const noteElement = createNoteElement(note.id, note.content);
+  notesContainer.insertBefore(noteElement, addNoteButton);
+});
 
