c4ebaa1043e4dc710e908a7c44f48d8b8fc6b091
MichaelCampbell2020 <cam3000@gmail.com>
2022-01-29 15:15:22 -0800

    sending saved note to local storage

--- main.js
+++ main.js
@@ -11,3 +11,8 @@ addNoteButton.addEventListener("click", () => addNoteButton());
 function getNotes() {
   return JSON.parse(localStorage.getItem("sticky-notes") || "[]");
 }
+
+function saveNotes(notes) {
+  localStorage.setItem("stickynotes-notes", JSON.stringify(notes));
+}
+
